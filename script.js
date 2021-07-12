8cfc1d4340d440f3f9ade5542bced59fc2d0ea22
Jose <74021467+jgarcia24@users.noreply.github.com>
2021-07-12 17:59:52 -0500

    Added necessary files for map

---
+++ script.js
@@ -0,0 +1,327 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoianJnYXJjaWEyNCIsImEiOiJja3F2aWgwOTIwNm1oMnhxeTd0cWgwYzlpIn0.aHh3w5v6KAnvkeBlKxRLNw';
+
+var geojson = {
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Madison Square Park",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "400 Lexington 78215"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.490376,
+                    29.434623
+                ],
+                "type": "Point"
+            },
+            "id": "0cead345da5db5e5e7ab14823fa0a92d"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Panther Springs Park",
+                "hours": "Sunday-Saturday: Sunrise to Sunset",
+                "location": "22635 Wilderness Oak 78258"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.512662,
+                    29.645481
+                ],
+                "type": "Point"
+            },
+            "id": "0f68610b8f023950133c970d07e187dd"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Travis Park",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "301 E. Travis, 78205"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.489125,
+                    29.428095
+                ],
+                "type": "Point"
+            },
+            "id": "13d537f117413a94bf0817fd788b80f9"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Phil Hardberger Park",
+                "hours": "Sunday-Saturday: Sunrise to Sunset",
+                "location": "8400 N.W. Military Hwy"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.528702,
+                    29.553962
+                ],
+                "type": "Point"
+            },
+            "id": "1a6c559b0703cea213173bc5132b9244"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Kingsborough Park",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "350 Felps 78221"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.49583,
+                    29.331114
+                ],
+                "type": "Point"
+            },
+            "id": "1dbb6ee197e775afa83044e22cf83be9"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Maverick Park",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "1000 Broadway 78215"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.479687,
+                    29.434441
+                ],
+                "type": "Point"
+            },
+            "id": "3017591939d02f6d35d9307ce93f3b1a"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "New Territories Recreational Complex",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "9023 Bowen 78250"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.670782,
+                    29.518356
+                ],
+                "type": "Point"
+            },
+            "id": "4783e2feb15510162f3df58f82dd039b"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Southside Lions Park",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "3100 Hiawatha, 78210"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.434672,
+                    29.384297
+                ],
+                "type": "Point"
+            },
+            "id": "4abd44bbb254e9b5dee601b3a30d9462"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Lady Bird Johnson Park",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "10700 Nacogdoches Road"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.425261,
+                    29.534372
+                ],
+                "type": "Point"
+            },
+            "id": "979772e4a26ef4090a6546560765d8e2"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Tom Slick Park",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "7400 Highway 151, 78227"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.635269,
+                    29.430053
+                ],
+                "type": "Point"
+            },
+            "id": "9eea044932c578acded98e2a84b34742"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "McAllister Park",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "13102 Jones-Maltsberger 78247"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.450759,
+                    29.558725
+                ],
+                "type": "Point"
+            },
+            "id": "ba169d6573c69d151ae453ef491922cf"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Oscar E. Perez Memorial Park",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "8601 Timber Path, 78250"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.656556,
+                    29.482286
+                ],
+                "type": "Point"
+            },
+            "id": "c6d480241249ce59b795eae0a389b57b"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Woodlawn Lake Park",
+                "hours": "Sunday-Saturday: 5 a.m. – Midnight",
+                "location": "1103 Cincinnati, 78201"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.531417,
+                    29.451724
+                ],
+                "type": "Point"
+            },
+            "id": "de6848fa4456d50e10ad93369ea3594e"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Falcone Park",
+                "hours": "Sunday-Saturday: 5 a.m. – 11 p.m",
+                "location": "7625 Mystic Park 78254"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.645873,
+                    29.523573
+                ],
+                "type": "Point"
+            },
+            "id": "e60e931845359a48458089f529e65fe6"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Pearsall Park",
+                "hours": "",
+                "location": "5102 Old Pearsall Road, 78242"
+            },
+            "geometry": {
+                "coordinates": [
+                    -98.593756,
+                    29.35155
+                ],
+                "type": "Point"
+            },
+            "id": "f08df6b4fd0e267386fe1863e14ddcbd"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    -98.3242539244704,
+                    29.56155224000389
+                ],
+                "type": "Point"
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Universal City Dog Park",
+                "hours": "Everyday 6:00 am - 11:00 pm",
+                "location": "134 Athenian Dr. Universal City, TX 78148"
+            }
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    -98.26784466216432,
+                    29.56684351889625
+                ],
+                "type": "Point"
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Schertz Dog Park",
+                "location": "940 Community Circle Dr."
+            }
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    -98.47666685747552,
+                    29.48300206987932
+                ],
+                "type": "Point"
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Alamo Heigths Bark Park",
+                "hours": "Sunrise to Sunset 7 days a week",
+                "location": "Alamo Heights Boulevard"
+            }
+        }
+    ],
+    "type": "FeatureCollection"
+}
+
+// initialize map
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-98.491142, 29.424349],
+    zoom: 10
+});
+
+// add source
+
+// add zoom functionality
+var nav = new mapboxgl.NavigationControl();
+map.addControl(nav);
+
+//direction functionality
+var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+});
+
+map.addControl(directions, 'top-left');
+
+// add markers to map
+geojson.features.forEach(function (marker) {
+
+    // create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML('<h2>' + marker.properties.name + '</h2>' + '<h3>' + marker.properties.location + '</h3>' + '<p>' + marker.properties.hours + '</p>'))
+        .addTo(map);
+});
